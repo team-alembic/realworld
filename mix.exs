b8cd44bbceeff44656a7a3ee57acbf9ae4b0f913
Will Tan <william.tanzy@gmail.com>
2023-01-31 14:14:04 +1100

    chore: remove unnecessary :gettext

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule Realworld.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
