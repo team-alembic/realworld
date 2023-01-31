a708264ac1bef0fb5f5433383d771306514c7eb2
Will Tan <william.tanzy@gmail.com>
2023-02-01 10:44:29 +1100

    chore: rename file from support.ex -> accounts.ex

--- lib/realworld/accounts/accounts.ex
+++ lib/realworld/accounts/accounts.ex
@@ -1,5 +1,9 @@
 defmodule Realworld.Accounts do
-  use Ash.Api
+  use Ash.Api, otp_app: :realworld
+
+  authorization do
+    authorize :by_default
+  end
 
   resources do
     registry Realworld.Accounts.Registry
