1278a29bbf15f8ea538dd3dcf9109dc239a0e40f
Will Tan <william.tanzy@gmail.com>
2023-02-01 10:45:23 +1100

    chore: upgrade phoenix to 0.18

--- lib/realworld_web.ex
+++ lib/realworld_web.ex
@@ -44,7 +44,7 @@ defmodule RealworldWeb do
 
   def live_view do
     quote do
-      use Phoenix.LiveView,
+      use Phoenix.Component,
         layout: {RealworldWeb.LayoutView, "live.html"}
 
       unquote(view_helpers())
@@ -90,7 +90,7 @@ defmodule RealworldWeb do
       use Phoenix.HTML
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
-      import Phoenix.LiveView.Helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Realworld.MixProject do
       {:phoenix, "~> 1.6.15"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.18.11"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
