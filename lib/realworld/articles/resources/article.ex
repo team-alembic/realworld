507c5997b7b834b942884238fdc30a75f73ff697
Nick Bosch <nick@alembic.com.au>
2023-02-10 12:07:33 +1100

    ref: improved article tag join resource

--- lib/realworld/articles/resources/article.ex
+++ lib/realworld/articles/resources/article.ex
@@ -20,11 +20,19 @@ defmodule Realworld.Articles.Article do
       primary? true
       accept [:title, :description, :body]
       argument :user, :uuid, allow_nil?: false
+      argument :tags, {:array, Realworld.Articles.Tag}, allow_nil?: true
+
+      change manage_relationship(:tags, on_lookup: :relate, on_no_match: :create)
 
       # ? confirm this approach
+      # ? should I be somehow reading the 'actor' here and using that as the user?
       change manage_relationship(:user, on_lookup: :relate, on_no_match: :error)
 
-      # ? confirm this approach
+      # change manage_relationship(:tags,
+      #          on_lookup: :relate,
+      #          on_no_match: {:create, :create, :create, [:article_id, :tag_id]}
+      #        )
+
       change Realworld.Articles.Changes.SlugifyTitle
     end
   end
@@ -63,7 +71,7 @@ defmodule Realworld.Articles.Article do
       allow_nil? false
     end
 
-    many_to_many :tag_list, Realworld.Articles.Tag do
+    many_to_many :tags, Realworld.Articles.Tag do
       through Realworld.Articles.ArticleTag
       source_attribute_on_join_resource :article_id
       destination_attribute_on_join_resource :tag_id
--- lib/realworld/articles/resources/article_tag.ex
+++ lib/realworld/articles/resources/article_tag.ex
@@ -2,26 +2,25 @@ defmodule Realworld.Articles.ArticleTag do
   use Ash.Resource,
     data_layer: AshPostgres.DataLayer
 
+  alias Realworld.Articles.Article
+  alias Realworld.Articles.Tag
+
   postgres do
     table "article_tags"
     repo Realworld.Repo
+
+    references do
+      reference :article, on_delete: :delete
+      reference :tag, on_delete: :delete
+    end
   end
 
   actions do
     defaults [:create, :read, :update, :destroy]
   end
 
-  attributes do
-    uuid_primary_key :id
-  end
-
   relationships do
-    belongs_to :article, Realworld.Articles.Article do
-      allow_nil? false
-    end
-
-    belongs_to :tag, Realworld.Articles.Tag do
-      allow_nil? false
-    end
+    belongs_to :article, Article, primary_key?: true, allow_nil?: false
+    belongs_to :tag, Tag, primary_key?: true, allow_nil?: false
   end
 end
