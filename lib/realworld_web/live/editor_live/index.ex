45e13dacb1e751b0ae795215cdf6efc801388c5c
Nick Bosch <nick@alembic.com.au>
2023-02-10 14:52:57 +1100

    feat: current user is actor

--- lib/realworld_web/live/editor_live/index.ex
+++ lib/realworld_web/live/editor_live/index.ex
@@ -1,12 +1,23 @@
 defmodule RealworldWeb.EditorLive.Index do
   use RealworldWeb, :live_view
 
+  alias Realworld.Accounts.User
   alias Realworld.Articles.Article
 
-  def mount(_params, _session, socket) do
-    form = AshPhoenix.Form.for_create(Article, :publish, api: Realworld.Articles)
+  def mount(_params, session, socket) do
+    socket = assign_user_id(socket, session)
+    user = Realworld.Accounts.get!(User, socket.assigns.user_id)
+
+    form = AshPhoenix.Form.for_create(Article, :publish, api: Realworld.Articles, actor: user)
 
     {:ok, assign(socket, form: form)}
+  end
+
+  defp assign_user_id(socket, session) do
+    case fetch_user_id(session) do
+      {:ok, user_id} -> assign(socket, :user_id, user_id)
+      {:error, reason} -> put_flash(socket, :error, reason)
+    end
   end
 
   def handle_event("validate", %{"form" => params}, socket) do
@@ -20,11 +31,27 @@ defmodule RealworldWeb.EditorLive.Index do
       {:ok, result} ->
         # Do something with the result, like redirect
         IO.inspect(result)
-        {:noreply, socket}
+        {:noreply, redirect(socket, to: "/article/#{result.slug}")}
 
       {:error, form} ->
         IO.inspect(form)
         {:noreply, assign(socket, :form, form)}
+    end
+  end
+
+  @spec fetch_user_id(map()) :: {:ok, String.t()} | {:error, any()}
+  defp fetch_user_id(session) do
+    case Map.fetch(session, "user") do
+      {:ok, user_param} ->
+        user_param =
+          String.split(user_param, "?")
+          |> List.last()
+          |> URI.decode_query()
+
+        {:ok, user_param["id"]}
+
+      :error ->
+        {:error, "User is not logged in"}
     end
   end
 end
