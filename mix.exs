536877a3e0913c4f53edbd2c87afd4f397789a8e
Zach Daniel <zach@zachdaniel.dev>
2024-05-25 06:52:29 -0400

    chore: update elixir requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Realworld.MixProject do
     [
       app: :realworld,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
